fd28c231b5d3f7080345e83cba899760108bd4ef
Alberto Costa de Araújo <albertobsb@outlook.com>
2022-01-13 10:56:47 -0300

    Add project files.

---
+++ Examples/CreateWord.cs
@@ -0,0 +1,33 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+
+namespace Examples
+{
+    public class CreateWord
+    {
+        public CreateWord()
+        {
+            var docPath = @"..\..\..\src\example.docx";
+            CreateWordDoc(docPath, "Hello, Word!");
+        }
+
+        public static void CreateWordDoc(string filepath, string msg)
+        {
+            using (WordprocessingDocument doc = WordprocessingDocument.Create(filepath, DocumentFormat.OpenXml.WordprocessingDocumentType.Document))
+            {
+                // Add a main document part. 
+                MainDocumentPart mainPart = doc.AddMainDocumentPart();
+
+                // Create the document structure and add some text.
+                mainPart.Document = new Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+                Paragraph para = body.AppendChild(new Paragraph());
+                Run run = para.AppendChild(new Run());
+
+                // String msg contains the text, "Hello, Word!"
+                run.AppendChild(new Text(msg));
+            }
+        }
+    }
+}
---
+++ OpenXmlStudy/Program.cs
@@ -0,0 +1,14 @@
+﻿using Examples;
+using System;
+
+namespace OpenXmlStudy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Criando documento word");
+            var createWord = new CreateWord();
+        }
+    }
+}
